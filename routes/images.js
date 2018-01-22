0e92536df081b2a293b0b51c055d90bf18da41a9
Tano Iannetta <tano.iannetta@heig-vd.ch>
2018-01-22 09:33:50 +0100

    ajout delay avant envoie de l image

--- routes/images.js
+++ routes/images.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+function sleep(seconds){
+  var waitUntil = new Date().getTime() + seconds*1000;
+  while(new Date().getTime() < waitUntil) true;
+}
+
+
 /* GET users listing. */
 router.get('/', (req, res) => {
 
   var file = Math.floor((Math.random() * 12) + 1);
-
+  sleep(2);
   res.sendFile(path.resolve(__dirname+'/../public/img/' + file +'.jpg'));
 });
 
