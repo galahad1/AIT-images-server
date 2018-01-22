4bf5f9454240ed6f194b5a5ee76db2612f9bc96e
Tano Iannetta <tano.iannetta@heig-vd.ch>
2018-01-22 11:03:08 +0100

    ajout endpoints pour avoir images définies

--- routes/images.js
+++ routes/images.js
@@ -16,4 +16,73 @@ router.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname+'/../public/img/' + file +'.jpg'));
 });
 
+
+
+router.get('/1', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/19.jpg'));
+});
+
+
+
+router.get('/2', (req, res) => {
+  res.sendFile(path.resolve(__dirname+'/../public/img/20.jpg'));
+});
+
+
+
+router.get('/3', (req, res) => {
+
+  var file = Math.floor((Math.random() * 25) + 1);
+
+res.sendFile(path.resolve(__dirname+'/../public/img/21.jpg'));
+});
+
+router.get('/4', (req, res) => {
+  res.sendFile(path.resolve(__dirname+'/../public/img/22.jpg'));
+
+});
+
+
+router.get('/5', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/23.jpg'));
+
+});
+
+
+
+router.get('/6', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/24.jpg'));
+
+});
+
+
+router.get('/7', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/25.jpg'));
+
+});
+
+router.get('/8', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/15.jpg'));
+
+});
+
+
+router.get('/9', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/16.jpg'));
+
+});
+
+
+router.get('/10', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname+'/../public/img/17.jpg'));
+
+});
+
 module.exports = router;
